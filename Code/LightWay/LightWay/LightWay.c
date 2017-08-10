0f980ed043bdb18fadb4c2036a1bdbb385b87099
Savio-Cardoz <cardoz.savio@gmail.com>
2017-08-10 17:59:50 +0530

    Modified to allow PIR run @ 3V

--- Code/LightWay/LightWay/LightWay.c
+++ Code/LightWay/LightWay/LightWay.c
@@ -14,32 +14,55 @@
 #define PIR_PIN 2
 #define LED_PIN	1
 
-#define LED_ON do{PORTB |= (1 << LED_PIN);} while (0)
-#define LED_OFF do{PORTB &= ~(1 << LED_PIN);} while(0)
-
 #define TIMER_VAL	15535		// with a clock running @ clk/1024, TIMER_VAL will give a time of 50ms
-#define LIGHT_TIMEOUT 1//1200		// 60 secs / 50ms	
+#define LIGHT_TIMEOUT 3//1200		// 60 secs / 50ms	
 
-volatile uint8_t pir_count;
+typedef struct			// Using to access individual bits/pins of a register/port
+{
+	unsigned int bit0:1;
+	unsigned int bit1:1;
+	unsigned int bit2:1;
+	unsigned int bit3:1;
+	unsigned int bit4:1;
+	unsigned int bit5:1;
+	unsigned int bit6:1;
+	unsigned int bit7:1;
+} _io_reg;
+
+#define REGISTER_BIT(rg,bt) ((volatile _io_reg*)&rg)->bit##bt
+
+#define SETBIT(ADDRESS,BIT) (ADDRESS |= (1<<BIT))
+#define CLEARBIT(ADDRESS,BIT) (ADDRESS &= ~(1<<BIT))
+#define CHECKBIT(ADDRESS,BIT) (ADDRESS & (1<<BIT))
+#define BIT_FLIP(ADDRESS,BIT) ((ADDRESS)^=(BIT))
+
+#define LED_ON SETBIT(PORTB, 1)
+#define LED_OFF CLEARBIT(PORTB, 1)
+
+volatile uint8_t pir_count, flag;
 volatile uint32_t timer_loop;
 uint16_t timer_load_val;
 
 ISR(INT0_vect)
 {
-	++pir_count;
+	pir_count += 1;
 	TCNT1 = TIMER_VAL;
-	TCCR1B |= (1 << CS12) | (1 << CS10);		// Start timer clock @ clk/1024 =>  1.024ms
-	
+	TCCR1B |= (1 << CS12);		// Start timer clock @ clk/1024 =>  1.024ms
+	LED_ON;
 }
 
 ISR(TIMER1_OVF_vect)
 {
-	//After overflowing the desired number of times procedd down
+	//After overflowing the desired number of times proceed down
 	++timer_loop;
+	TCNT1 = TIMER_VAL;
 	if(timer_loop > LIGHT_TIMEOUT)
 	{
 		TCCR1B = 0x00;		// stop all timer clock
 		pir_count = 0;	
+		cli();
+		LED_OFF;
+		_delay_ms(10000);
 	}
 	
 	//go to sleep
@@ -54,11 +77,13 @@ void setup_controller()
 	PORTB &= ~(1 << LED_PIN);		// Keep LED OFF
 	
 	GICR |= (1 << INT0);					// Enable interrupt on INT0 pin
-	MCUCR |= (1 << ISC00) | (1 << ISC01);	// Rising edge of INT0 will trigger an interrupt
+	CLEARBIT(MCUCR, ISC00);
+	CLEARBIT(MCUCR, ISC01);
 	
 	ACSR |= (1 << ACD);		// Disable the analog comparator to reduce power consumption
 	ADCSRA &= ~(1 << ADEN);	// Ensuring ADC is kept OFF to reduce power consumption
 	
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 }
 
 void init_timer()
@@ -67,31 +92,22 @@ void init_timer()
 	TCCR1B = 0x00;				// Clock is off
 	TIMSK |= (1 << TOIE1);		// Overflow interrupt enabled
 	TCNT1 = TIMER_VAL;			// time for 50ms @ clk/1024 
-	
-	TCCR1B |= (1 << CS12) | (1 << CS10);
 }
 
 int main(void)
 {
-	
 	setup_controller();
 	init_timer();
 	pir_count = 0;
 	sei();
 	
-	LED_ON;
-	
     while(1)
     {
 		if(pir_count == 0)
 		{
-			LED_OFF;
-			// sleep;
-		}
-		
-		if(pir_count > 0)
-		{
-			LED_ON;
+			sleep_enable();
+			sleep_cpu();
+			sleep_disable();
 		}
     }
 }
